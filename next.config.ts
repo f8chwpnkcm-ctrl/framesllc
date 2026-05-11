e2e8d02aaaf9053516a0226afb08105783af2036
Matthew Ory <framesbymatt@gmail.com>
2026-05-11 16:17:48 -0400

    downgrade to next 14, fix config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,3 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  reactCompiler: false,
-};
-
-export default nextConfig;
+echo "/** @type {import('next').NextConfig} */
+const nextConfig = {}
+module.exports = nextConfig" > next.config.js
